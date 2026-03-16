8f31d7624dcd5b6418b4ddb99b6cf7a649e9d3c4
Another_One <hd.tetuan5002@gmail.com>
2026-03-15 19:33:36 -0600

    Prueba ssh

---
+++ Conexión.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#Comandos para hacer subir los cambios al repositorio
+echo "Git status"
+echo "-------------------"
+git status
+echo "-------------------"
+echo "Git add all"
+echo "-------------------"
+#git add .
+echo "-------------------"
+echo "Git commit -m $1"
+echo "-------------------"
+#git commit -m $1
+echo "-------------------"
+echo "Git push"
+#git push
+
